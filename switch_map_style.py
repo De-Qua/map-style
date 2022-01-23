770ff4ce1ec0d51405dd184595d1f6763b2c86fa
lucatastrophe <luca.deltorre9@gmail.com>
2022-01-24 00:29:16 +0100

    nuova versione dello stile in dequa2022

--- switch_map_style.py
+++ switch_map_style.py
@@ -4,12 +4,12 @@ import json
 with open('basic_style.json') as json_basic:
     basic_data = json.load(json_basic)
 
-with open('dequa_style.json') as json_dequa:
+with open('dequa2022.json') as json_dequa:
     dequa_data = json.load(json_dequa)
 
 dequa_correct_data= [layer for layer in dequa_data['layers'] if layer['id'] is not "natural_earth_shaded_relief" or layer['id'] is not "natural_earth"]
 pdb.set_trace()
 basic_data['layers'] = dequa_correct_data
 
-with open('style.json', 'w') as json_new:
+with open('style2022.json', 'w') as json_new:
     json.dump(basic_data, json_new)
